8500745cd6a7afbd7edfb15bafaea4aa5c2a870c
ajaycarloss <ajaypulikkal940@gmail.com>
2025-10-17 23:36:20 +0530

    porrchangea

--- ajay-portfolio/src/modules/__tests__/smoke.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import { App } from '../App';
-
-test('renders hero name', () => {
-  render(<App />);
-  expect(screen.getByText(/Ajay Pulikkal/i)).toBeInTheDocument();
-});
